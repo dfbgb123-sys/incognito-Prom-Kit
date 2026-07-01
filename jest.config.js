747989aae4bcbc6131f08de2cb15117d17e8f3e8
dfbgb123-sys <dfbgb123@gmail.com>
2026-07-01 16:31:21 +0900

    fix: resolve ts-node missing error in CI by adding ts-node and migrating to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
@@ -15,5 +13,3 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/$1',
   },
 };
-
-export default config;
